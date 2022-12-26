1ecc1a487e227c39f7aa8760601303ff16465a0c
lzq123 <a17859989913@163.com>
2022-12-26 17:50:46 +0800

    first commit

--- src/main/java/com/my/blog/website/controller/HelloController.java
+++ src/main/java/com/my/blog/website/controller/HelloController.java
@@ -1,6 +1,10 @@
 package com.my.blog.website.controller;
 
+import com.my.blog.website.service.IAttachService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @Author 86178
@@ -8,4 +12,13 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class HelloController {
+
+    @Autowired
+    private IAttachService attachService;
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public void process(){
+
+    }
 }
