14b47c4b176405008492c25a9743b109209081c6
lzq123 <a17859989913@163.com>
2022-12-26 17:45:15 +0800

    first commit

---
+++ src/main/java/com/my/blog/website/controller/HelloController.java
@@ -0,0 +1,11 @@
+package com.my.blog.website.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Author 86178
+ * @create 2022/12/26 17:44
+ */
+@Controller
+public class HelloController {
+}
